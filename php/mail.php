b3781c1570a056a95bae382662a836be15d16415
David Guscic <david.guscic@gmail.com>
2024-04-23 16:39:34 +0200

    logika za mail provjera1

--- php/mail.php
+++ php/mail.php
@@ -1,12 +1,15 @@
 <?php
 
 if(isset($_POST['submit'])){
+
+    $ime_honey = $_POST['name'];
+    $prezime_honey = $_POST['surname'];
+
+    if($ime_honey==""){
     
     $to = "david.guscic@gmail.com"; // kome se šalje
     $from = $_POST['mail']; // tko šalje
     $full_name = $_POST['ime'];
-    
-    
     $subject = "Apartmani upit sa stranice";
     $message = $full_name . " želi rezervirati apartman od " . $_POST['from-date'] ." do ". $_POST['to-date'] . "\n piše sljedeće:\n\n" . $_POST['message'];
 
@@ -19,7 +22,10 @@ if(isset($_POST['submit'])){
       } else {
         echo "Email sending failed, try again or contact us manually...";
       }
-    
+    }
+    else{
+        echo "GRRRRRR";
+    }
 }
 
     ?>
