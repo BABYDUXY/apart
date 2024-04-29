1432da3cd3a7c0fc8689bfeccbb38fe0941f3527
David Guscic <david.guscic@gmail.com>
2024-04-29 20:13:53 +0200

    Prevedene stranice, još treba uredit linkove i stavit slike

--- php/mail.php
+++ php/mail.php
@@ -7,22 +7,22 @@ if(isset($_POST['submit'])){
 
     if($ime_honey=="" && $prezime_honey==""){
     
-    $to = "david.guscic@gmail.com"; // kome se šalje
-    $from = $_POST['mail']; // tko šalje
-    $full_name = $_POST['ime'];
-    $subject = "Apartmani upit sa stranice";
-    $message = $full_name . " želi rezervirati apartman od " . $_POST['from-date'] ." do ". $_POST['to-date'] . "\n piše sljedeće:\n\n" . $_POST['message'];
+      $to = "david.guscic@gmail.com"; // kome se šalje
+      $from = $_POST['mail']; // tko šalje
+      $full_name = $_POST['ime'];
+      $subject = "Apartmani upit sa stranice";
+      $message = $full_name . " želi rezervirati apartman od " . $_POST['from-date'] ." do ". $_POST['to-date'] . "\n piše sljedeće:\n\n" . $_POST['message'];
 
-    $headers = "From:" . $from;
-    
-    
+      $headers = "From:" . $from;
+      
+      
 
-    if (mail($to,$subject,$message,$headers)) {
-      header("Location: ../index.html");
-      } else {
-        echo "Email sending failed, try again or contact us manually...";
+      if (mail($to,$subject,$message,$headers)) {
+        header("Location: ../index.html");
+        } else {
+          echo "Email sending failed, try again or contact us manually...";
+        }
       }
-    }
     else{
         header("Location: ../index.html");
     }
