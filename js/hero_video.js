b5ff73c763ae7af5cccccb81c86cba40a124e05a
David Guscic <babyduxy@gmail.com>
2026-02-09 23:40:47 +0100

    dodano onclick na strelicu da otvori novu sekciju, za iphone mob nije consistent ali radi

--- js/hero_video.js
+++ js/hero_video.js
@@ -1,6 +1,26 @@
 const video = document.getElementById("heroVideo");
 
 let hasPlayedOnce = false;
+const scrollBtn = document.getElementById("scrollDownBtn");
+const targetSection = document.getElementById("white_div_apt");
+
+let isScrolling = false;
+
+scrollBtn.addEventListener("click", () => {
+  if (isScrolling) return; // sprječava višestruke click-ove
+
+  isScrolling = true;
+  const offset = 100;
+  window.scrollTo({
+    top: targetSection.offsetTop + offset,
+    behavior: "smooth",
+  });
+
+  // reset scroll lock nakon 600ms (vrijeme animacije)
+  setTimeout(() => {
+    isScrolling = false;
+  }, 600);
+});
 
 const tryPlay = () => {
   video.play().catch(() => {});
