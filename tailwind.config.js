ce5ef0ea5b6211687c15ebb1bb3093aa3b2ed8f7
David Guscic <babyduxy@gmail.com>
2026-02-28 16:41:02 +0100

    Dodano vertikalno gibanje valova, swipe up na meniju i prilagođene brzine i udaljenosti pomicanja strelica i swipe teksta ovisno o veličini mobilnog ekrana (manja udaljenost za manji ekran)

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,22 @@ module.exports = {
           "0%, 100%": { transform: "translateX(0px)" },
           "50%": { transform: "translateX(-30px)" },
         },
+        swipeXSmall: {
+          "0%, 100%": { transform: "translateX(0px)" },
+          "50%": { transform: "translateX(-40px)" },
+        },
+        swipeX2Small: {
+          "0%, 100%": { transform: "translateX(0px)" },
+          "50%": { transform: "translateX(-20px)" },
+        },
+        swipeY2: {
+          "0%, 100%": { transform: "translateY(0px) " },
+          "50%": { transform: "translateY(-30px) " },
+        },
+        swipeY2Small: {
+          "0%, 100%": { transform: "translateY(0px) " },
+          "50%": { transform: "translateY(-20px) " },
+        },
         wave: {
           "0%": { transform: "translateX(0) " },
           "50%": { transform: "translateX(-40%) " },
@@ -44,7 +60,13 @@ module.exports = {
       animation: {
         swipeX: "swipeX 2s ease-in-out infinite",
         swipeX2: "swipeX2 2s ease-in-out infinite",
-        wave: "wave 10s  ease-in-out infinite",
+        swipeY2: "swipeY2 2s ease-in-out infinite",
+        /* Za short breakpoint */
+        swipeXSmall: "swipeXSmall 2s ease-in-out infinite",
+        swipeX2Small: "swipeX2Small 2s ease-in-out infinite",
+        swipeY2Small: "swipeY2Small 2s ease-in-out infinite",
+        /* ------------------ */
+        wave: "wave 13s  ease-in-out infinite",
         waveBob: "waveBob 4s ease-in-out infinite",
       },
     },
